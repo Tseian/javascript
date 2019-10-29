cb2e7ffc12d4276aa1aa029588040a3cdf919827
xieweicheng <xieweicheng@globalegrow.com>
2019-10-29 19:55:43 +0800

    update

---
+++ aloJS/sort/relativeSortArray.js
@@ -0,0 +1,17 @@
+// /**
+//  * @param {number[]} arr1
+//  * @param {number[]} arr2
+//  * @return {number[]}
+//  */
+// var relativeSortArray = function (arr1, arr2) {
+//     let res = {};
+//     let yuxia = [];
+//     for (let i = 0; i < arr1.length; i++) {
+//         if(res[arr1]) 
+//     }
+//     console.log(arr1)
+//     return res;
+// };
+
+// let arr2 = [2, 1, 4, 3, 9, 6], arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19];
+// console.log(relativeSortArray(arr1, arr2))
