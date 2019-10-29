7a2bddc90f6c32d0b6c75b80f2a1cab4282f0ed3
xieweicheng <xieweicheng@globalegrow.com>
2019-10-29 13:01:47 +0800

    update

---
+++ nodejs/fs/stat.js
@@ -0,0 +1,16 @@
+// const fs = require("fs")
+// fs.stat('stat.js', (err, stats) => {
+//     if (err) throw err;
+//     console.log(`文件属性: ${JSON.stringify(stats)}`);
+// });
+
+
+// fs.open(Buffer.from('stat.js'), 'r', (err, fd) => {
+//     console.log("fd", fd)
+//     if (err) throw err;
+//     fs.close(fd, (err) => {
+//         if (err) throw err;
+//     });
+// });
+
+ 
