e8d80f45f76e32f4bb0ac00b9372298205c83044
tseian <tseianxwc@gmail.com>
2019-11-21 22:41:03 +0800

    update

--- nodejs/childProcess/win_exec.js
+++ nodejs/childProcess/win_exec.js
@@ -3,8 +3,7 @@ const { spawn, exec, fork } = require("child_process");
 // 执行完才将stdout stderr 返回给回调函数
 // 没有option的情况先可以使用文件作文第一个command参数
 // 衍生一个shell(cmd)并在shell中执行dir(windows)
-exec('dir', {
-    cwd: 'D:\\',
+exec('ls', {
     maxBuffer: 1024 * 1024, // stdout 最大输出
     // encoding: "unicode",
     // shell: '/usr/bin/bash' //衍生shell的程序文件 windows cmd
@@ -16,3 +15,8 @@ exec('dir', {
     console.log(stdout)
     console.log(stderr)
 });
+
+var cp = require('child_process');
+
+var child = cp.spawn('echo', ['你好', "钩子"]); // 执行命令
+child.stdout.pipe(process.stdout);
