b74b7c476591f46c19b6a866c932b0b1f16edd02
xieweicheng <xieweicheng@globalegrow.com>
2019-12-04 18:58:49 +0800

    update

--- aloJS/list/flatten.js
+++ aloJS/list/flatten.js
@@ -12,19 +12,45 @@
  * @return {Node}
  */
 var flatten = function (head) {
-    let res = {};
-    let resH = res;
-
+    let pre = null;
     let queue = [head];
     while (queue[0]) {
         let s = queue.pop();
         if (s) {
-            resH.next = s;
-            s.prev = resH;
-            resH = resH.next;
-            if (s.next) queue.push(s.next);
-            if (s.child) queue.push(s.child);
+            s.prev = pre;
+            if (pre) pre.next = s;
+            pre = s;
+            if (s.next) {
+                queue.push(s.next);
+            }
+            if (s.child) {
+                queue.push(s.child);
+                s.child = null;
+            }
+            s.next = null;
         }
     }
-    return res.next;
+    return head;
+};
+
+var flatten_ = function (head) {
+    let cur = head;
+    let pre = null;
+    while (cur) {
+        cur.prev = pre;
+        if (pre) pre.next = cur;
+        pre = cur;
+        if (cur.child) {
+            
+            cur.next.prev = null;
+            cur.next = null;
+
+            let tem = cur;
+            cur = cur.child;
+            tem.child = null;
+        } else {
+            cur = cur.next;
+        }
+    }
+    return head;
 };
